a4e9566c6f78b553023632ab6b420272698c1225
koosungyoon <gusungyoon123@naber.com>
2023-01-17 22:08:26 +0900

    Add multiply function
    calculator.py supports 3 functions now

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 #기본 계산기
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
+def add(a, b):
+    return a + b
+def subrator(a, b):
+    return a - b
+
+def multply(a, b):
+    return a * b
